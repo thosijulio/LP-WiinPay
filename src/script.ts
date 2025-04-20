a5463d2a4297b3e7685e81fa4bfba76900fdecd4
thosijulio <thosijulio@gmail.com>
2025-04-20 19:22:05 -0300

    subheros

--- src/script.ts
+++ src/script.ts
@@ -6,5 +6,19 @@ const includeHTML = async (id: string, filePath: string) => {
   }
 };
 
-includeHTML("header", "/partials/header.html");
-includeHTML("hero", "/partials/hero.html");
+Promise.all([
+  includeHTML("header", "/partials/header.html"),
+  includeHTML("hero", "/partials/hero.html"),
+  includeHTML("strips-texts", "/partials/strips-texts.html"),
+  includeHTML("subhero-1", "/partials/subhero-1.html"),
+  includeHTML("subhero-2", "/partials/subhero-2.html"),
+])
+  .then(() => {
+    const animationScript = document.createElement("script");
+    animationScript.src = "/dist/scrollAnimations.js";
+
+    document.body.insertAdjacentElement("beforeend", animationScript);
+  })
+  .catch(() => {
+    console.error("Erro ao inserir HTMLs");
+  });
---
+++ src/scrollAnimations.ts
@@ -0,0 +1,31 @@
+const TARGETS_IDS: string[] = [
+  "#subhero-1-texts",
+  "#subhero-1-card-1",
+  "#subhero-1-card-2",
+  "#subhero-1-card-3",
+  "#subhero-1-button",
+]; // INSIRA AQUI OS IDS DESEJADOS PARA AS ANIMAÇÕES DE SCROLL
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("display-element");
+      } else {
+        entry.target.classList.remove("display-element");
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+    //rootMargin: "0px 0px -100px 0px", // margem negativa no BOTTOM, exige mais scroll
+  }
+);
+
+TARGETS_IDS.forEach((targetId) => {
+  const target = document.querySelector(targetId);
+  console.log(target);
+  if (target) {
+    observer.observe(target);
+  }
+});
