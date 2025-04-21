ec08df2b285101429a933254273d46391542d129
thosijulio <thosijulio@gmail.com>
2025-04-21 12:02:54 -0300

    FEAT - desktop

--- src/script.ts
+++ src/script.ts
@@ -7,22 +7,22 @@ const includeHTML = async (id: string, filePath: string) => {
 };
 
 Promise.all([
-  includeHTML("header", "./partials/header.html"),
-  includeHTML("hero", "./partials/hero.html"),
-  includeHTML("strips-texts-1", "./partials/strips-texts.html"),
-  includeHTML("subhero-1", "./partials/subhero-1.html"),
-  includeHTML("subhero-2", "./partials/subhero-2.html"),
-  includeHTML("subhero-3", "./partials/subhero-3.html"),
-  includeHTML("subhero-4", "./partials/subhero-4.html"),
-  includeHTML("strips-texts-2", "./partials/strips-texts.html"),
-  includeHTML("faq", "./partials/faq.html"),
+  includeHTML("header", "/partials/header.html"),
+  includeHTML("hero", "/partials/hero.html"),
+  includeHTML("strips-texts-1", "/partials/strips-texts.html"),
+  includeHTML("subhero-1", "/partials/subhero-1.html"),
+  includeHTML("subhero-2", "/partials/subhero-2.html"),
+  includeHTML("subhero-3", "/partials/subhero-3.html"),
+  includeHTML("subhero-4", "/partials/subhero-4.html"),
+  includeHTML("strips-texts-2", "/partials/strips-texts.html"),
+  includeHTML("faq", "/partials/faq.html"),
 ])
   .then(() => {
     const scrollAnimationScript = document.createElement("script");
     const faqAnimationScript = document.createElement("script");
 
-    scrollAnimationScript.src = "./scrollAnimations.js";
-    faqAnimationScript.src = "./faqAnimation.js";
+    scrollAnimationScript.src = "/scrollAnimations.js";
+    faqAnimationScript.src = "/faqAnimation.js";
 
     document.body.insertAdjacentElement("beforeend", scrollAnimationScript);
     document.body.insertAdjacentElement("beforeend", faqAnimationScript);
